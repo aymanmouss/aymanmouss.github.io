a47fb74714d2f6ba483c7c121a0f805279dcd38f
Ayman Moussa <ayman@moussa.uk>
2021-11-15 12:05:16 +0100

    js

--- js/photographersPage.js
+++ js/photographersPage.js
@@ -1,16 +1,23 @@
 const card = document.getElementsByClassName("card");
 const photos = document.getElementsByClassName("photos");
 const idLocation = window.location.search.split("?id=")[1];
+const modal = document.querySelector(".modal");
+const total = document.getElementsByClassName("total");
+import LightBoxClass from "./lightBox.js";
+import FormModal from "./form.js";
+import Likes from "./Likes.js";
+import TotalLikes from "./totalLikes.js";
 
 // finding the type of the nedia
+
 const mediatype = (data) => {
   if (data.type == "image") {
     return `
-            <img src="../medias2/${data.photographerId}/${data.image}" alt="" class="photo">
+            <img src="../medias2/${data.photographerId}/${data.image}" alt="" class="photo" id=${data.id}>
             `;
   } else {
     return `
-            <video controls class="photo">
+            <video controls class="photo" id=${data.id}>
 
             <source src="../medias2/${data.photographerId}/${data.video}" type="video/mp4">
 
@@ -27,7 +34,7 @@ class photographersPage {
     
         <section class="user-card">
             <article class="photographers-information">
-                <h1>${data.name}</h1>
+                <h1 class='test1'>${data.name}</h1>
                 <p class="location">${data.city}, ${data.country}</p>
                 <p class="work">${data.tagline}</p>
                 <ul class="tags-card">
@@ -38,8 +45,7 @@ class photographersPage {
                       .join("")}
                 </ul>
             </article>
-            <a href="#" class="contact-me">Contactez-moi</a>
-            
+            <a href="#" class="contact-me"  >Contactez-moi</a>
         </section>
         <aside class="photo-id">
             <img class="photographers-photo" src="./medias/Photographers ID Photos/${
@@ -55,7 +61,10 @@ class photographersPage {
             ${mediatype(data)}
             <section class="photo-data">
                 <p class="photo-name">${data.title}</p>
-                <p class="likes">${data.likes} <i class="fas fa-heart"></i></p>
+                <p class="likes"> <span class='photoLikes' target=${
+                  data.likes
+                }> ${data.likes}</span>  
+                  <i class="fas fa-heart"></i></p>
             </section>
         </article>
       `;
@@ -69,12 +78,38 @@ fetch("./photographers.json")
     data.photographers.map((item) => {
       if (item.id == idLocation) {
         card[0].innerHTML += new photographersPage().cardlist(item);
+        // class TotalLikes
+        new TotalLikes(item);
       }
     });
     //   Media list
+
     data.media.map((item) => {
       if (item.photographerId == idLocation) {
         photos[0].innerHTML += new photographersPage().photoLsit(item);
       }
     });
+    // *********************************************
+
+    new Likes(data);
+
+    // *********************************************
+
+    // LightBox
+    new LightBoxClass();
+    // Form
+    data.photographers.map((item) => {
+      if (item.id == idLocation) {
+        modal.innerHTML = new FormModal().modal(item);
+      }
+    });
+
+    let titleList = [];
+
+    data.media.map((item) => {
+      if (item.photographerId == idLocation) {
+        titleList.push(item.title);
+      }
+    });
+    console.log(titleList.sort());
   });
