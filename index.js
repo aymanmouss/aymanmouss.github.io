4bf014725fc79304ed38ac71768c2a4452cbfecd
Ayman Moussa <ayman@moussa.uk>
2021-11-20 22:42:17 +0100

    js

---
+++ index.js
@@ -0,0 +1,19 @@
+import photographerslist from "./js/homePage/photographerslist.js";
+import filter from "./js/homePage/filter.js";
+import scrollid from "./js/homePage/scroll.js";
+const photographerslist1 =
+  document.getElementsByClassName("photographers-list");
+
+fetch("./photographers.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // Filte Nav Tags Class
+    new filter().filterTags(data);
+    scrollid();
+    // photographers List Class
+    data.photographers.map((data) => {
+      photographerslist1[0].innerHTML += new photographerslist().photograph(
+        data
+      );
+    });
+  });
