c05424a4c27e8cd9459c20d1bb63f8a975cf0569
Ayman Moussa <ayman@moussa.uk>
2021-11-20 12:46:34 +0100

    js

---
+++ js/photographersPages/formData.js
@@ -0,0 +1,35 @@
+let firstName = "";
+let lastName = "";
+let email = "";
+let message = "";
+const modal = document.querySelector(".modal");
+const formModal = document.querySelector("#formModal");
+
+export default class FormData {
+  constructor() {
+    window.addEventListener("change", (e) => {
+      if (e.target.id == "name") {
+        firstName = e.target.value;
+      }
+      if (e.target.id == "lastname") {
+        lastName = e.target.value;
+      }
+      if (e.target.id == "email") {
+        email = e.target.value;
+      }
+      if (e.target.id == "message") {
+        message = e.target.value;
+      }
+    });
+    window.addEventListener("click", (e) => {
+      if (e.target.classList.value == "btn-submit") {
+        console.log(`Prénom :${firstName}`);
+        console.log(`Nom :${lastName}`);
+        console.log(`Email :${email}`);
+        console.log(`Message :${message}`);
+
+        modal.classList.add("displaynone");
+      }
+    });
+  }
+}
